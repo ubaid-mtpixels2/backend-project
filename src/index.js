8f788da892a9b6662b4c78a818f200f67657ef72
ubaid-mtpixels2 <ubaid.mtpixels@gmail.com>
2025-02-07 19:10:39 +0500

    configured express app, created a standardized API Error and Response Classes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: '/.env'
-})
-connectDB()
-
- 
+  path: "/.env",
+});
+connectDB().then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log('Server is running on PORT ', process.env.PORT || 8000 );
+        
+    } )
+}).catch((err)=>{
+    console.log("Mongo DB Connection Failed !! ", err);
+    
+});
